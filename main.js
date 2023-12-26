8a60d86cf4b75dd94c2ec1b58a5c9559db8aa51c
wenkipan <1024381676@qq.com>
2023-12-26 20:34:13 +0800

    second

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ app.use(express.json())
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
-app.get('/email/deliver', async (req, res) => {
+app.get('/express/deliver', async (req, res) => {//jijian
     const { sender_name, sender_number, sender_phone, sender_ID, sender_type, sender_addr, sender_payway } = req.body
     const { receiver_name, receiver_phone, receiver_addr } = req.body
     const { item_type, item_length, item_width, item_height, item_cost } = req.body
     const [rows, fields] = await connection.execute(
-        ' INSERT INTO VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,)',
+        ' INSERT INTO deliver VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,)',
         [sender_name, sender_number, sender_phone, sender_ID, sender_type, sender_addr, sender_payway,
             receiver_name, receiver_phone, receiver_addr,
             item_type, item_length, item_width, item_height, item_cost]
@@ -32,6 +32,23 @@ app.get('/email/deliver', async (req, res) => {
     res.send({ status: 1, msg: "ok", data: rows, })
 
 })
+
+
+app.get('/express/receive', async (req, res) => {
+    const { mail_number } = req.body
+    const [rows, fields] = await connection.execute(
+        'SELECT * FROM deliver WHERE mail_number = ?',
+        [mail]
+    );
+    if (rows == null) {
+        res.send({ status: 0, msg: "database inserting err", data: '', })
+        return
+    }
+
+    res.send({ status: 1, msg: "ok", data: rows, })
+
+})
+
 
 
 app.listen(3000)
